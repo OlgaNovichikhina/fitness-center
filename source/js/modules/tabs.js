0cc3f1dbc771dab87c259fed058efd879cd51da7
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-12-08 23:40:46 +0300

    стилизация табов

--- source/js/modules/tabs.js
+++ source/js/modules/tabs.js
@@ -2,9 +2,11 @@
 const TAB_BUTTON = document.querySelectorAll('.subscription__nav-button');
 const TAB_NAV = document.querySelector('.subscription__nav');
 const TAB_CONTENT = document.querySelectorAll('.subscription__subcontent');
+const SUB_TITLE = document.querySelectorAll('[data-sub-title]');
 
 function hideTabContent(tab) {
   for (let i = tab; i < TAB_CONTENT.length; i++) {
+    SUB_TITLE[tab].style.display = 'flex';
     TAB_CONTENT[i].classList.remove('subscription__subcontent--active');
     TAB_BUTTON[i].classList.remove('subscription__nav-button--active');
     TAB_CONTENT[i].classList.add('subscription__subcontent--close');
@@ -14,6 +16,7 @@ function hideTabContent(tab) {
 
 function showTabContent(tab) {
   if (!TAB_CONTENT[tab].classList.contains('subscription__subcontent--active')) {
+    SUB_TITLE[tab].style.display = 'none';
     TAB_CONTENT[tab].classList.add('subscription__subcontent--active');
     TAB_BUTTON[tab].classList.add('subscription__nav-button--active');
     TAB_CONTENT[tab].classList.remove('subscription__subcontent--close');
@@ -23,6 +26,7 @@ function showTabContent(tab) {
 
 function onClickTab() {
   hideTabContent(1);
+  SUB_TITLE[0].style.display = 'none';
   TAB_NAV.classList.remove('no-js');
   TAB_NAV.addEventListener('click', function (evt) {
     evt.preventDefault();
